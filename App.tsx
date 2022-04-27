773018b1bb3bfd1af2fc5c672f26cb5b1de7cbbd
develcode01 <michael.junges@develcode.com.br>
2022-04-27 11:20:20 -0300

    Bottom tab configurado

--- App.tsx
+++ App.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TelaA } from './src/screens/TelaA';
 import { TelaB } from './src/screens/TelaB';
 
 export default function App() {
+    const Stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
 
-  const Stack = createNativeStackNavigator();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Tela A" component={TelaA} />
-        <Stack.Screen name="Tela B" component={TelaB} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Tela A" component={TelaA} />
+                <Tab.Screen name="Tela B" component={TelaB} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
