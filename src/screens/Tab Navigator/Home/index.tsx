dfeaec6cc9a77a856fe84076caa9ef26762209b2
develcode01 <michael.junges@develcode.com.br>
2022-04-28 15:40:25 -0300

    criando metodo post (axios)

--- src/screens/Tab Navigator/Home/index.tsx
+++ src/screens/Tab Navigator/Home/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useFetch } from '../../../services/useFetch';
 import { Container, Title } from './styles';
+import { ActivityIndicator, Button } from 'react-native';
+import { usePost } from '../../../services/usePost';
 
 export function Home({ navigation }: any) {
-
-    const { data, loading, error } = useFetch('/53370300/json/')
+    const { data, loading, error } = useFetch('/95032050/json/');
 
     useEffect(() => {
         console.log(data.logradouro);
@@ -13,10 +14,33 @@ export function Home({ navigation }: any) {
         console.log(data.uf);
     }, [data]);
 
-    return (
+    return loading ? (
+        <ActivityIndicator size={'large'} />
+    ) : (
         <Container>
-            <Title>Início</Title>
-            <Title>{data.logradouro} {"\n"} {"\n"} {data.bairro} {"\n"} {"\n"} { data.localidade }</Title>
+            <Title>get</Title>
+            <Title>
+                {data.logradouro} {'\n'} {'\n'} {data.bairro} {'\n'} {'\n'}
+                {data.localidade}
+            </Title>
+
+            <Title>post</Title>
+            <Title></Title>
+            <Button
+                title="enviar"
+                onPress={() => {
+                    usePost(
+                        '/public/v2/users',
+                        {
+                            email: '',
+                            gender: '',
+                            name: '',
+                            status: '',
+                        },
+                        {}
+                    );
+                }}
+            />
         </Container>
     );
 }
