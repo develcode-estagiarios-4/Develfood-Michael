e5cfe8e3fd9d4ea77120aeca499339f77d38b886
Michael Junges <michael.junges@develcode.com.br>
2022-06-30 14:20:50 -0300

    alteraçoes na scrollview da checkoutpage

--- src/screens/TabNavigator/Historic/index.tsx
+++ src/screens/TabNavigator/Historic/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text } from 'react-native'
+import { AuthContext } from '@context/auth';
 import { Container } from './styles'
+import { useFetch } from '@services/useFetch';
 
 export function Historic({ navigation }: any) {
+
+    const {userId, token} = useContext(AuthContext);
+
+    const { data, fetchData } = useFetch(
+        `/request/costumer?id=${userId}&page=0&quantity=10`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    );
+
     return (
         <Container>
             <Text style={{ fontSize: 36 }}>Histórico</Text>
+
         </Container>
     )
 }
